e636d233c5c32102e431e889321692a5913dcc72
Axait-th <axaitth@gmail.com>
2025-12-18 22:06:07 +0500

    .

---
+++ src/TimeMultiplier.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TimeMultiplier = () => {
+  return (
+    <div>this component display little X2 on click it  multiple tim eand after specific time it start adding time. </div>
+  )
+}
+
+export default TimeMultiplier
